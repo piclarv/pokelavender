6f712211d5b28abe2454a18e66eac8ef81950c4c
piclarv <piclarv@gmail.com>
2015-01-13 05:20:59 -0500

    Wrote fresh ASM code to generate maps in place of binary blk data.

---
+++ maps/redshouse1f.asm
@@ -0,0 +1,17 @@
+; This can be used to replace the .blk  ;
+; files for easier viewing and editing. ;
+; The blocks are arranged in the same   ;
+; shape as the map, in this case 4 x 4  ;
+; but could easily be the shape of any  ;
+; map you are working on.  More common  ;
+; blocks like empty floor could be      ;
+; assigned constant names (i.e.         ;
+; TALL_GRASS or EMPTY_FLOOR)            ;
+;                             - piclarv ;
+;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;
+
+RedsHouse1FBlocks:
+	db $04, $09, $05, $07
+	db $0f, $0f, $0f, $0f
+	db $0f, $01, $02, $0f
+	db $0f, $0b, $0f, $0f
