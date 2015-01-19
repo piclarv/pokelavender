6b51672afb0f4ee1f4b74a0ca4730559f699e799
piclarv <piclarv@gmail.com>
2015-01-19 08:56:07 -0500

    remove debug code from testing rollout

--- data/wildPokemon/route2.asm
+++ data/wildPokemon/route2.asm
@@ -13,37 +13,27 @@ Route2Mons:
 		db 7,PIDGEY
 		db 7,PIDGEY
 	ELSE
-;		db 3,RATTATA
-;		db 3,PIDGEY
-;		db 4,PIDGEY
-;		db 4,RATTATA
-;		db 5,PIDGEY
-;		if DEF(_RED)
-;			db 3,WEEDLE
-;		ENDC
-;		if DEF(_BLUE)
-;			db 3,CATERPIE
-;		ENDC
-;		db 2,RATTATA
-;		db 5,RATTATA
-;		if DEF(_RED)
-;			db 4,WEEDLE
-;			db 5,WEEDLE
-;		ENDC
-;		if DEF(_BLUE)
-;			db 4,CATERPIE
-;			db 5,CATERPIE
-;		ENDC
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
-		db 14,NIDORINO
+		db 3,RATTATA
+		db 3,PIDGEY
+		db 4,PIDGEY
+		db 4,RATTATA
+		db 5,PIDGEY
+		if DEF(_RED)
+			db 3,WEEDLE
+		ENDC
+		if DEF(_BLUE)
+			db 3,CATERPIE
+		ENDC
+		db 2,RATTATA
+		db 5,RATTATA
+		if DEF(_RED)
+			db 4,WEEDLE
+			db 5,WEEDLE
+		ENDC
+		if DEF(_BLUE)
+			db 4,CATERPIE
+			db 5,CATERPIE
+		ENDC
 	ENDC
 	db $00
 
